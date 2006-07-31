1b144d35da3504969472017afdf2596a50937db0
progoth <progoth@c5207f5d-e219-0410-bfa0-a769f592ecd4>
2006-07-31 15:57:09 +0000

    checking in the test scripts I whipped up for my own uses
    they were written while in a car somewhere in tennessee or kentucky,
    and combined with the mod_php and apache I thought to apt-get before
    I left, made for a really useful devel environment.  If I had had
    a DC adaptor for my laptop, I might have finished this thing:)

---
+++ misc/phptest/index.php
@@ -0,0 +1,52 @@
+<?
+
+header( "Content-type: text/plain" );
+
+function hs_uptodate(){
+?>
+UPTODATE
+57575757575757575757575757575757
+http://localhost/~progoth/v1.1.php
+INTERVAL 6
+<?
+}
+
+function hs_update(){
+?>
+UPDATE http://localhost
+57575757575757575757575757575757
+http://localhost/~progoth/v1.1.php
+INTERVAL 6
+<?
+}
+
+function hs_baduser(){
+?>
+BADUSER
+INTERVAL 6
+<?
+}
+
+function hs_failed(){
+?>
+FAILED not written yet yo
+INTERVAL 3
+<?
+}
+
+function unparse(){
+?>
+uteaonhutoansuhoaeu
+eau
+aou
+oa
+u
+<?
+}
+
+if( strtolower( $_GET["u"] ) == "progothdevtest" )
+    hs_uptodate();
+else
+    hs_baduser();
+
+?>
